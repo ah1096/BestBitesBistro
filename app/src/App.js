0cfb9c7f289d85e67f4bd430a3b68accbc9fa936
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-12 21:14:24 +0000

    header is working, added cat icon

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./Navbar.js";
 export default function App(){
     return (
         <div>
-            <h1>insert title here</h1>
             <Navbar />
         </div>
     )
--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,7 +1,49 @@
+import fillercat from './cat-icon.png';
+
+
 export default function Navbar () {
    return ( 
         <div>
-            <h1>return something</h1>
+            
+            <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+        <img src={fillercat} alt="placeholder cat" className="headerIcon"></img>
+        
+        best BITES bistro</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">'bout</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">bites</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">buddies</a>
+        </li>
+        
+        <li className="nav-item">
+          <a className="nav-link" href="#">buy</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
         </div>
     )
 }
